bc8784ca018ffe1aa413e2ca851647d26ac044c0
Pericles001 <periclesadjovi@outlook.fr>
2021-05-28 09:25:34 +0100

    imnplement second chqllenge solution

--- 07 - Array Cardio Day 2/index.js
+++ 07 - Array Cardio Day 2/index.js
@@ -21,11 +21,9 @@ const people = [
 
   const older = (elem)=> (date.getFullYear() - elem.year) >= 19;
 
-
-
   console.log(people.some(older))
   // Array.prototype.every() // is everyone 19 or older?
-
+console.log(people.every(older))
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
