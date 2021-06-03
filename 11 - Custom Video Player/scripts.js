e1f256dec3454f557aad1533204aaa9a1714b15e
Pericles001 <periclesadjovi@outlook.fr>
2021-06-03 15:40:19 +0100

    tryinmg to delete elements from table

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,14 +1,19 @@
-let grades =  [-5,105,95,75,05,15,25,35,45,55,65]
+let grades = [-5, 105, 95, 75, 05, 15, 25, 35, 45, 55, 65];
 
+function gradingStudents(arr) {
+  arr.forEach((elem, index) => {
+    if (elem < 38 && arr[index=1] < 38) {
+      arr.splice(index, 2);
+    }else if(elem < 38){
+        arr.splice(index, 1);
+    }
+  });
 
-function gradingStudents(arr){
-arr.forEach((elem, index)=>{
-   if(elem < 38){
-       arr.splice(index, 1)
-   }
-   console.log(arr)
-})
-return 'try'
+  return "try";
 }
 
-gradingStudents(grades)
+gradingStudents(grades);
+/*
+
+
+  */
