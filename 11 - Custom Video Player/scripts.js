ab68aa9777391b78c0c14a400e1c3d4ca315c536
Pericles001 <periclesadjovi@outlook.fr>
2021-06-03 15:41:16 +0100

    correct bad syntaxing

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,11 +2,12 @@ let grades = [-5, 105, 95, 75, 05, 15, 25, 35, 45, 55, 65];
 
 function gradingStudents(arr) {
   arr.forEach((elem, index) => {
-    if (elem < 38 && arr[index=1] < 38) {
+    if (elem < 38 && arr[index+1] < 38) {
       arr.splice(index, 2);
     }else if(elem < 38){
         arr.splice(index, 1);
     }
+    console.table(elem, arr[index+1])
   });
 
   return "try";
