b6eb32cc237b9d2039d03e0c19724240dd251511
Pericles001 <periclesadjovi@outlook.fr>
2021-06-03 09:37:40 +0100

    last push

--- 10 - Hold Shift and Check Checkboxes/index.js
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -5,6 +5,18 @@
     const multiCheck = (el, e)=>{
         if(lastSelected && e.shiftKey){
             el.checked = lastSelected.checked
+
+            const [startIdx, endIdx] = el.id > lastSelected.id ?
+            [lastSelected.id, el.id] :
+            [el.id, lastSelected.id]
+
+            const middleInputs = [...checkBoxes].slice(parseInt(startIdx )+1,endIdx )
+
+            middleInputs.forEach(checkBox => checkBox.checked = el.checked)
+
+            lastSelected = el
         }
     }
+
+    checkBoxes.forEach()
 })
