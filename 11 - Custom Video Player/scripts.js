10510db8204aa77deec91f977bd815b4ccf12e19
Pericles001 <periclesadjovi@outlook.fr>
2021-06-03 15:50:05 +0100

    found a new issue

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,14 +1,16 @@
 let grades = [-5, 105, 95, 75, 05, 15, 25, 35, 45, 55, 65];
 
 function gradingStudents(arr) {
-  arr.forEach((elem, index) => {
-    if (elem < 38 && arr[index+1] < 38) {
-      arr.splice(index, 2);
-    }else if(elem < 38){
-        arr.splice(index, 1);
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] < 38){
+
     }
-    console.table(elem, arr[index+1])
-  });
+}
+
+
+ 
+  console.table(arr)
 
   return "try";
 }
