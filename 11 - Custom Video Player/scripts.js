023a16dd7a208bd1530764b5f7a9cf8692fd8000
Pericles001 <periclesadjovi@outlook.fr>
2021-06-03 15:57:57 +0100

    succe3ssfully used recursivity on array to delete unneccessary occurrences

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,16 +1,24 @@
-let grades = [-5, 105, 95, 75, 05, 15, 25, 35, 45, 55, 65];
+let grades = [-5, 105, 95, 75, 05, 15, 25, 35, 45, 55, 65, 87, 17, 67];
 
 function gradingStudents(arr) {
-
-for(let i = 0; i < arr.length; i++){
-    if(arr[i] < 38){
-
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 38) {
+      arr.forEach((elem, index) => {
+        console.table(elem, arr[index + 1]);
+        if (elem < 38 && arr[index + 1] < 38) {
+          arr.splice(index, 2);
+        } else if (elem < 38) {
+          arr.splice(index, 1);
+        } else if (arr[index + 1] < 38) {
+          arr.splice(index + 1, 1);
+        } else {
+          console.log("hourra clean");
+        }
+      });
     }
-}
+  }
 
-
- 
-  console.table(arr)
+  console.table(arr);
 
   return "try";
 }
