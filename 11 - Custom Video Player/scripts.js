3b79a6c690b2682db41ee2ace95bdf0fcad107c5
Pericles001 <periclesadjovi@outlook.fr>
2021-06-11 22:18:56 +0100

    test for hackerrank challenge

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,26 +1,22 @@
 let grades = [-5, 105, 95, 75, 05, 15, 25, 35, 45, 55, 65, 87, 17, 67];
 
 function gradingStudents(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < 38) {
-      arr.forEach((elem, index) => {
-        console.table(elem, arr[index + 1]);
-        if (elem < 38 && arr[index + 1] < 38) {
-          arr.splice(index, 2);
-        } else if (elem < 38) {
-          arr.splice(index, 1);
-        } else if (arr[index + 1] < 38) {
-          arr.splice(index + 1, 1);
-        } else {
-          console.log("hourra clean");
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 38) {
+            arr.forEach((elem, index) => {
+
+                if (elem < 38 && arr[index + 1] < 38) {
+                    arr.splice(index, 2);
+                } else if (elem < 38) {
+                    arr.splice(index, 1);
+                } else if (arr[index + 1] < 38) {
+                    arr.splice(index + 1, 1);
+                }
+            });
         }
-      });
     }
-  }
 
-  console.table(arr);
-
-  return "try";
+    return (arr);
 }
 
 gradingStudents(grades);
