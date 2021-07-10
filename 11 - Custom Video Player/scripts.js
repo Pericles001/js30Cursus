9815353fb0ccc9c4320ad0b6551b4477a5e9cc07
Pericles001 <periclesadjovi@outlook.fr>
2021-07-10 13:20:29 +0100

    finish handleProgress function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -60,11 +60,33 @@ function togglePlay() {
 }
 
 function updateButton() {
-    console.log('update the button')
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
 }
 
+function skip(){
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+    video[this.name] = this.value;
+  
+}
+
+function handleProgress(){
+    const percent = (video.currentTime / video.duration)* 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
 /* Hook up the event listeners */
 
 video.addEventListener('click', togglePlay);
-video.addEventListener('play', togglePlay)
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate ));
+
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate ));
